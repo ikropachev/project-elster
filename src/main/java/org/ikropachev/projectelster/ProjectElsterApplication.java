272a70c32015738baf790c5d4ae29adbe40011a5
Ivan Kropachev <88405779+ikropachev@users.noreply.github.com>
2023-08-16 21:15:42 +0300

    Initialization

---
+++ src/main/java/org/ikropachev/projectelster/ProjectElsterApplication.java
@@ -0,0 +1,13 @@
+package org.ikropachev.projectelster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectElsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectElsterApplication.class, args);
+	}
+
+}
